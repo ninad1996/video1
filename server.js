82a85d84e2a83aeb83beac4b0aca0e618ce69ddd
ninad <ninad.sabnis16@gmail.com>
2018-05-02 11:32:58 +0530

    First commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path= require('path');
 app.get('/', function(req, res) {
-res.sendFile('public/index.html', {root: __dirname});
+res.sendFile('public/index.html' , { root : __dirname});
 });
 
 //Whenever someone connects this gets executed
@@ -17,8 +17,4 @@ io.on('connection', function(socket) {
       console.log('Ninad is Offline');
    });
 });
-
-
-http.listen(3000,'0.0.0.0'|| 'localhost', function() {
-    console.log('Listening to port:  ' + 3000);
-});
+http.listen(3000, () => console.log('Server running on port 3000'))
